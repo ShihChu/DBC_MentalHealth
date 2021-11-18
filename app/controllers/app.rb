d0f157f17dceb10c549d5d95d8498dc948e54bc0
ShihChu <juju0411tw@gmail.com>
2021-11-18 10:30:28 +0800

    Fix change page problem

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 require 'roda'
-require 'slim'
 require 'html'
 require 'yaml'
 
 module MentalHealth
   # Web App
   class App < Roda
-    #plugin :render, engine: 'slim', views: 'app/views'
     plugin :render, engine: 'html.erb', views: 'app/views'
     plugin :assets, css: 'style.css', path: 'app/views/assets'
     plugin :public, root: 'app/views/public'
@@ -21,6 +19,14 @@ module MentalHealth
       # GET /
       routing.root do
         view 'home', engine: 'html.erb'
+      end
+
+      routing.on 'meditation' do
+        routing.is do
+          routing.get do
+            view 'meditation', engine: 'html.erb'
+          end
+        end
       end
 
       # routing.on 'report' do
